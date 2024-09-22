f2493919079002af4a44700c79304434ff240376
Mohamed El-Qarram <mohamed0ibrahim0000@gmail.com>
2024-09-22 18:54:09 +0300

    Add reference in the same solution

---
+++ EmployeeData/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace EmployeeData
+{
+    public class Employee :Human
+    {
+        static int id;
+        decimal salary;
+        
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get {return age; } set { age = value; }}
+        public int ID { get; private set;}
+        public decimal Salary {set { salary = value; } get { return salary; } }
+        public Gender Gender { get {return gender;} }
+
+         static Employee()
+        {
+            id = 0;
+        }
+        public Employee() : this(string.Empty,0,0,0 ) { }
+        public Employee(string name, decimal salary, int age, Gender gender):base(name , age , gender)
+        {
+            id++;
+            ID = id;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+$"ID: {ID}\nSalary: {salary}\n";
+        }
+    }
+    public enum Gender
+    {
+        Male,Female
+    }
+}
---
+++ EmployeeData/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace EmployeeData
+{
+    public class Human
+    {
+        protected string name;
+        protected int age;
+        protected readonly Gender gender;
+
+        public Human():this("" , 0 , Gender.Male) { }
+        public Human(string name, int age, Gender gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}\nAge: {age}\nGender: {gender}\n";
+        }
+
+    }
+}
